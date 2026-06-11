b027807ca40758caa815d89783b6772651c21466
ashishkunthe <ashishkunthe2003@gmail.com>
2026-06-11 14:41:36 +0530

    added the auth parts

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,1 +1,40 @@
-export function authMiddleware() {}
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+interface requestExtended extends Request {
+  userId: string;
+}
+
+export function authMiddleware(
+  req: requestExtended,
+  res: Response,
+  next: NextFunction
+) {
+  const token = req.headers.authorization;
+
+  try {
+    if (!token) {
+      res.status(403).json({
+        message: "No token found pls login",
+      });
+      return;
+    }
+
+    const decode = jwt.verify(token, process.env.JWT_SECRET as string) as {
+      userId: string;
+    };
+
+    if (!decode) {
+      res.json({ message: "Invalid credentials" });
+      return;
+    }
+
+    req.userId = decode.userId;
+    next();
+  } catch (error) {
+    console.log("Error in auth middleware", error);
+    res.status(500).json({
+      message: "Something went wrong",
+    });
+  }
+}
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,93 @@
 import { Router } from "express";
+import { loginTypes, registerTypes } from "../types/authTypes";
+import { User } from "../models/User";
+
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const route = Router();
 
-route.post("/regiter");
+route.post("/regiter", async (req, res) => {
+  const inputs = registerTypes.safeParse(req.body);
 
-route.post("/login");
+  if (!inputs.success) {
+    res.status(402).json({
+      messages: "Invalid inputs",
+    });
+    return;
+  }
+
+  try {
+    const { email, password, username } = inputs.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      email,
+      username,
+      password: hashedPassword,
+    });
+
+    const token = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_SECRET as string
+    );
+
+    res.status(201).json({
+      message: "User registration completed",
+      token: token,
+    });
+  } catch (error) {
+    console.log("Error in registration of user", error);
+    res.json({
+      message: "Something went wrong",
+    });
+  }
+});
+
+route.post("/login", async (req, res) => {
+  const inputs = loginTypes.safeParse(req.body);
+
+  if (!inputs.success) {
+    res.json({
+      message: "Invalid inputs",
+    });
+    return;
+  }
+
+  try {
+    const { email, password } = inputs.data;
+    const findUser = await User.findOne({ email });
+
+    if (!findUser) {
+      res.status(404).json({
+        message: "User is not registered",
+      });
+      return;
+    }
+
+    const decode = await bcrypt.compare(password, findUser.password);
+
+    if (!decode) {
+      res.status(403).json({
+        message: "Invalid Password",
+      });
+      return;
+    }
+
+    const token = jwt.sign(
+      { userId: findUser._id },
+      process.env.JWT_SECRET as string
+    );
+
+    res.status(200).json({
+      message: "Login sucessful",
+      token: token,
+    });
+  } catch (error) {
+    console.log("Error in login ", error);
+    res.status(500).json({
+      message: "Something went wrong",
+    });
+  }
+});
 
 export default route;
